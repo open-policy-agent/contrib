a368c66b91b74ae1b89abe20ed330d1255ac0b41
Josh Marshall <joshua.r.marshall.1991@gmail.com>
2019-02-25 13:54:53 -0500

    Applied black formatting.
    Signed-off-by: Josh Marshall <joshua.r.marshall.1991@gmail.com>

--- api_authz/docker/echo_server.py
+++ api_authz/docker/echo_server.py
@@ -10,6 +10,7 @@ import requests
 
 import logging
 import sys
+
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
 app = Flask(__name__)
@@ -17,12 +18,9 @@ app = Flask(__name__)
 opa_url = os.environ.get("OPA_ADDR", "http://localhost:8181")
 policy_path = os.environ.get("POLICY_PATH", "/v1/data/httpapi/authz")
 
+
 def check_auth(url, user, method, url_as_array, token):
-    input_dict = {"input": {
-        "user": user,
-        "path": url_as_array,
-        "method": method,
-    }}
+    input_dict = {"input": {"user": user, "path": url_as_array, "method": method}}
     if token is not None:
         input_dict["input"]["token"] = token
 
@@ -34,17 +32,21 @@ def check_auth(url, user, method, url_as_array, token):
         logging.info(err)
         return {}
     if rsp.status_code >= 300:
-        logging.info("Error checking auth, got status %s and message: %s" % (j.status_code, j.text))
+        logging.info(
+            "Error checking auth, got status %s and message: %s"
+            % (j.status_code, j.text)
+        )
         return {}
     j = rsp.json()
     logging.info("Auth response:")
     logging.info(json.dumps(j, indent=2))
     return j
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
 def root(path):
-    user_encoded = request.headers.get('Authorization', "Anonymous:none")
+    user_encoded = request.headers.get("Authorization", "Anonymous:none")
     if user_encoded:
         user_encoded = user_encoded.split("Basic ")[1]
     user, _ = base64.b64decode(user_encoded).decode("utf-8").split(":")
@@ -54,7 +56,12 @@ def root(path):
     j = check_auth(url, user, request.method, path_as_array, token).get("result", {})
     if j.get("allow", False) == True:
         return "Success: user %s is authorized \n" % user
-    return "Error: user %s is not authorized to %s url /%s \n" % (user, request.method, path)
+    return "Error: user %s is not authorized to %s url /%s \n" % (
+        user,
+        request.method,
+        path,
+    )
+
 
 if __name__ == "__main__":
     app.run()
