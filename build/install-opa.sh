227bd57d4912f93bb954823e909897f5031348b5
Torin Sandall <torinsandall@gmail.com>
2019-02-27 16:55:53 -0800

    Update Travis CI to install opa into PATH
    Signed-off-by: Torin Sandall <torinsandall@gmail.com>

---
+++ build/install-opa.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -exo pipefail
+
+curl https://api.github.com/repos/open-policy-agent/opa/releases/latest |
+    grep 'browser_download_url.*linux_amd64' |
+    cut -d : -f 2,3 |
+    tr -d '"' |
+    wget -qi -
+
+chmod u+x opa_linux_amd64
+ln -s opa_linux_amd64 opa
