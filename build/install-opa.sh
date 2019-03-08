1a109f193a92b58428a69a930441e613fe4a92c5
Torin Sandall <torinsandall@gmail.com>
2019-03-08 08:13:15 -0800

    Fix OPA install script to follow redirects
    Enable verbose logging on curl because command is failing in Travis
    but not locally.
    
    Signed-off-by: Torin Sandall <torinsandall@gmail.com>

--- build/install-opa.sh
+++ build/install-opa.sh
@@ -2,7 +2,7 @@
 
 set -exo pipefail
 
-curl https://api.github.com/repos/open-policy-agent/opa/releases/latest |
+curl -vL https://api.github.com/repos/open-policy-agent/opa/releases/latest |
     grep 'browser_download_url.*linux_amd64' |
     cut -d : -f 2,3 |
     tr -d '"' |
