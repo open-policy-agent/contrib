f6a257e670c884e70283e29c9b868743666d16d3
Urvil Patel <patelurvil38@gmail.com>
2019-05-28 14:21:42 +0530

    Add opa pkg, contains http client to interact with opa
    Signed-off-by: Urvil Patel <patelurvil38@gmail.com>

---
+++ opa-iptables/pkg/opa/opa.go
@@ -0,0 +1,51 @@
+package opa
+
+import (
+	"errors"
+)
+
+type Client interface {
+	Data
+	Policy
+}
+
+type Data interface {
+	GetData(path string) error
+	PutData(path string, data interface{}) error
+	PatchData(path string, op string, data interface{}) error
+}
+
+type Policy interface {
+	InsertPolicy(id string, data []byte) error
+	DeletePolicy(id string) error
+}
+
+type httpClient struct {
+	url string
+	prefix string
+	authentication string
+}
+
+func New(url string, auth string) Client {
+	return &httpClient{url,"",auth}
+}
+
+func (c *httpClient) InsertPolicy(id string, data []byte) error {
+	return errors.New("Not implemented")
+}
+
+func (c *httpClient) DeletePolicy(id string) error {
+	return errors.New("Not implemented")
+}
+
+func (c *httpClient) GetData(path string) error {
+	return errors.New("Not implemented")
+}
+
+func (c *httpClient) PatchData(path string,op string, data interface{}) error {
+	return errors.New("Not implemented")
+}
+
+func (c *httpClient) PutData(path string, data interface{}) error {
+	return errors.New("Not implemented")
+}
