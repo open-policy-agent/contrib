7b8597826a671bbf4dd5f363c7df536a9ea22cd9
Urvil Patel <patelurvil38@gmail.com>
2019-06-05 23:31:41 +0530

    correct import path
    Signed-off-by: Urvil Patel <patelurvil38@gmail.com>

--- opa-iptables/main.go
+++ opa-iptables/main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"flag"
 
-	"github.com/contrib/opa-iptables/pkg/logging"
+	"github.com/open-policy-agent/contrib/opa-iptables/pkg/logging"
 	"github.com/sirupsen/logrus"
 )
 
 var logger *logrus.Logger
 
 func main() {
-	OpaEndpoint := flag.String("opa-endpoint","127.0.0.1:8181","endpoint of opa in form of ip:port i.e. 192.33.0.1:8181")
+	OpaEndpoint := flag.String("opa-endpoint","http://127.0.0.1:8181","endpoint of opa in form of ip:port i.e. 192.33.0.1:8181")
 	LogFormat := flag.String("log-format","json","set log format. i.e. text | json | json-pretty")
 	LogLevel := flag.String("log-level","info","set log level. i.e. info | debug | error")
 
