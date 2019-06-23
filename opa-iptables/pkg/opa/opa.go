7ab3f1aab89e872e4df0f3a9753c93482f2b6df0
Urvil Patel <patelurvil38@gmail.com>
2019-06-23 22:16:01 +0530

    Add OPA client to query document
    Signed-off-by: Urvil Patel <patelurvil38@gmail.com>

--- opa-iptables/pkg/opa/opa.go
+++ opa-iptables/pkg/opa/opa.go
@@ -1,50 +1,92 @@
 package opa
 
 import (
-	"errors"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
 )
 
+const (
+	documentEndpointFmt = `/v1/data/%s`
+)
+
+// Error contains the standard error fields returned by OPA.
+type Error struct {
+	Code    string          `json:"code"`
+	Message string          `json:"message"`
+	Errors  json.RawMessage `json:"errors,omitempty"`
+}
+
+func (err *Error) Error() string {
+	return fmt.Sprintf("code %v: %v", err.Code, err.Message)
+}
+
 type Client interface {
-	Data
-	Policy
+	Query
 }
 
-type Data interface {
-	GetData(path string) error
-	PutData(path string, data interface{}) error
-	PatchData(path string, op string, data interface{}) error
-}
-
-type Policy interface {
-	InsertPolicy(id string, data []byte) error
-	DeletePolicy(id string) error
+type Query interface {
+	DoQuery(path string, input interface{}) (data []byte, err error)
 }
 
 type httpClient struct {
-	url string
+	opaEndpoint    string
 	authentication string
+	client         *http.Client
 }
 
-func New(url string, auth string) Client {
-	return &httpClient{url,auth}
+func New(opaEndpoint string, auth string) Client {
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+	return &httpClient{opaEndpoint, auth, client}
 }
 
-func (c *httpClient) InsertPolicy(id string, data []byte) error {
-	return errors.New("Not implemented")
+func (c *httpClient) DoQuery(path string, input interface{}) (data []byte, err error) {
+	url := c.opaEndpoint + fmt.Sprintf(documentEndpointFmt, path)
+	d, ok := input.([]byte)
+	if !ok {
+		return nil, fmt.Errorf("Invalid data; must be []byte")
+	}
+	res, err := c.do(http.MethodPost, url, d)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
 }
 
-func (c *httpClient) DeletePolicy(id string) error {
-	return errors.New("Not implemented")
+func (c *httpClient) do(method, url string, data []byte) ([]byte, error) {
+	req, err := http.NewRequest(method, url, bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Content-Type", "application/json")
+	if c.authentication != "" {
+		req.Header.Add("Authorization", "Bearer "+c.authentication)
+	}
+	res, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	err = c.handleErrors(res)
+	if err != nil {
+		return nil, err
+	}
+	return ioutil.ReadAll(res.Body)
 }
 
-func (c *httpClient) GetData(path string) error {
-	return errors.New("Not implemented")
-}
-
-func (c *httpClient) PatchData(path string,op string, data interface{}) error {
-	return errors.New("Not implemented")
-}
-
-func (c *httpClient) PutData(path string, data interface{}) error {
-	return errors.New("Not implemented")
+func (c *httpClient) handleErrors(resp *http.Response) error {
+	if resp.StatusCode >= 200 && resp.StatusCode < 300 {
+		return nil
+	}
+	var err Error
+	if err := json.NewDecoder(resp.Body).Decode(&err); err != nil {
+		return err
+	}
+	return &err
 }
