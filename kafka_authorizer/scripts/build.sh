c84a7e59ef3b64b592cb0d54b7ac8146cd65a04d
Anders Eknert <anders@eknert.com>
2021-02-02 16:46:57 +0100

    Add --quiet flag to kafka build
    Was printing thousands of lines in the build output.
    With --quiet will only print errors.
    
    Signed-off-by: Anders Eknert <anders@eknert.com>

--- kafka_authorizer/scripts/build.sh
+++ kafka_authorizer/scripts/build.sh
@@ -2,4 +2,4 @@
 
 DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
-docker run -w /src -v $DIR/..:/src maven:3.5.3-jdk-8 mvn install
+docker run -w /src -v $DIR/..:/src maven:3.5.3-jdk-8 mvn --quiet install
