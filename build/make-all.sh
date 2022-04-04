5b92f919235fa67764d7cbab2babdcc22ee0a0a9
Charles Daniels <charlesdaniels@users.noreply.github.com>
2022-04-04 15:54:30 -0400

    build cross-platform images for envoy_iptables (#166)
    * build cross-platform images for envoy_iptables
    
    Signed-off-by: Charles Daniels <charles@styra.com>

--- build/make-all.sh
+++ build/make-all.sh
@@ -11,6 +11,18 @@ fi
 
 CONTRIB_ROOT=$(dirname "${BASH_SOURCE}")/..
 
+# This works around the error:
+#
+#	Error while loading /usr/local/sbin/dpkg-split: No such file or directory
+#
+# Which can occur when using `docker buildx` to perform multi-architecture
+# builds in GitHub Actions.
+#
+# See: https://github.com/docker/buildx/issues/495#issuecomment-761562905
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+docker buildx create --name multiarch --driver docker-container --use
+docker buildx inspect --bootstrap
+
 for file in ${CONTRIB_ROOT}/*/Makefile; do
 
     dir=$(dirname "$file")
